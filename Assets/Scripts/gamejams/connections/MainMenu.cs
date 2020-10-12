dcd9bf77dfea2731368e8152a42c4512c17da906
Zhi Fei <zhifei_lee@aceatt.com.my>
2020-10-12 22:47:19 +0800

    - Completed win game state

--- Assets/Scripts/gamejams/connections/MainMenu.cs
+++ Assets/Scripts/gamejams/connections/MainMenu.cs
@@ -2,14 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
 
 namespace com.zhifez.seagj {
   public class MainMenu : Base {
-    
+    public Transform camTarget;
+    public float initPanAngle = 45;
+    public float panAngle = 15f;
+    public float panDuration = 3f;
 
     //--------------------------------------------------
     // private
     //--------------------------------------------------
+    private void RotateCamTarget () {
+      Vector3 _nextAngle = new Vector3 ( 0f, initPanAngle + panAngle, 0f );
+      camTarget.DORotate ( 
+        _nextAngle, panDuration
+      )
+      .SetEase ( Ease.Linear )
+      .OnComplete ( () => {
+        _nextAngle.y = initPanAngle - panAngle;
+        camTarget.DORotate ( 
+          _nextAngle, 
+          panDuration 
+        )
+        .SetEase ( Ease.Linear )
+        .OnComplete ( () => {
+          RotateCamTarget ();
+        } );
+      } );
+    }
 
     //--------------------------------------------------
     // public
@@ -18,8 +40,12 @@ namespace com.zhifez.seagj {
     //--------------------------------------------------
     // protected
     //--------------------------------------------------
-    protected void Awake () {
-			// play audio
+    protected void Start () {
+			// TODO: play audio
+      Vector3 _camAngle = new Vector3 ( 0f, initPanAngle, 0f );
+      _camAngle.y -= panAngle;
+      camTarget.eulerAngles = _camAngle;
+      RotateCamTarget ();
     }
 
     protected void Update () {
