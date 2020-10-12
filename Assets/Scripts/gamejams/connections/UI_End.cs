473728a0c1fce8ad62d8c6b019fa74b59486fdd5
Zhi Fei <zhifei_lee@aceatt.com.my>
2020-10-12 22:51:44 +0800

    - Added scene loading to menu

--- Assets/Scripts/gamejams/connections/UI_End.cs
+++ Assets/Scripts/gamejams/connections/UI_End.cs
@@ -276,7 +276,7 @@ namespace com.zhifez.seagj {
 
     private void State_game_over () {
       if ( Input.GetKeyDown ( KeyCode.Space ) ) {
-        // SceneManager.LoadScene ( "menu" );
+        SceneManager.LoadScene ( "menu" );
       }
 
       if ( Input.GetKeyDown ( KeyCode.R ) ) {
